6461217c49ceccb218678119ef602c9d674328b3
Anna <tolstovaaniuta@yandex.ru>
2022-07-13 16:57:37 +0300

    Task 1_1

--- 1_Homework/1_1/Program.cs
+++ 1_Homework/1_1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Write a number a : ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number b : ");
+int b = int.Parse(Console.ReadLine());
+
+if (a>b)
+{
+       Console.WriteLine(a);
+}
+
+else
+{
+       Console.WriteLine(b);
+}
--- 1_Homework/1_2/Program.cs
+++ 1_Homework/1_2/Program.cs
@@ -4,14 +4,15 @@ int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Write a number b : ");
 int b = int.Parse(Console.ReadLine());
 
+Console.WriteLine("Write a number c : ");
+int c = int.Parse(Console.ReadLine());
+
 if (a>b)
 {
-     Console.WriteLine(a);
+       Console.WriteLine(a);
 }
 
-else 
-
+else
 {
-     Console.WriteLine(b);
+       Console.WriteLine(b);
 }
-
