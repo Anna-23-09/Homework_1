d8b58adf6361723daed4b6fc2e70af9239b753e5
Anna <tolstovaaniuta@yandex.ru>
2022-07-14 13:37:32 +0300

    Task 1_5

---
+++ 1_Homework/1_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Write a number a : ");
+int a = int.Parse(Console.ReadLine());
+
+a % 10 = b;
+{
+       Console.WriteLine(b);
+}
+
+
---
+++ 1_Homework/1_5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Write a number a : ");
+int a = int.Parse(Console.ReadLine());
+
+int b = a%10;
+
+{
+       Console.WriteLine(b);
+}
