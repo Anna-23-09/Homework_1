c9313b85f23c1b6ce0bbdae10528dc3bae957903
Anna <tolstovaaniuta@yandex.ru>
2022-07-14 14:09:13 +0300

    Task 1_4

--- 1_Homework/1_4/Program.cs
+++ 1_Homework/1_4/Program.cs
@@ -1,9 +1,13 @@
-﻿Console.WriteLine("Write a number a : ");
+﻿
+Console.WriteLine("Write a number a : ");
 int a = int.Parse(Console.ReadLine());
 
-a % 10 = b;
-{
-       Console.WriteLine(b);
-}
+//  if (i <= a)
+int i = 2;
+while (i <= a)
 
+{
+     Console.WriteLine(i);
+     i+=2;
+}
 
