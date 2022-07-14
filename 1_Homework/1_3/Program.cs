8b4dda84209e58de62c13d53c49d4f08c3c67d65
Anna <tolstovaaniuta@yandex.ru>
2022-07-14 12:54:01 +0300

    Task 1_3

---
+++ 1_Homework/1_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write a number a : ");
+int a = int.Parse(Console.ReadLine());
+
+
+if (a % 2 == 0)
+{
+       Console.WriteLine("Да");
+}
+ else
+ {
+       Console.WriteLine("Нет");
+}
