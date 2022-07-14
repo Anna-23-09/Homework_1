e748b08b54598efcc707a2e9ea6b60a281b8c9bf
Anna <tolstovaaniuta@yandex.ru>
2022-07-14 12:49:12 +0300

    Task 1_2

--- 1_Homework/1_2/Program.cs
+++ 1_Homework/1_2/Program.cs
@@ -7,12 +7,23 @@ int b = int.Parse(Console.ReadLine());
 Console.WriteLine("Write a number c : ");
 int c = int.Parse(Console.ReadLine());
 
-if (a>b)
+if(a>b)
 {
        Console.WriteLine(a);
 }
-
+        if(a>c)
+       {
+           Console.WriteLine(a);
+       }
 else
 {
        Console.WriteLine(b);
 }
+      if(b>c)
+      {
+       Console.WriteLine(b);
+      }
+    else 
+    {
+       Console.WriteLine(c);
+      }
